282be5affd232ebe9558b751ef663307dc7382a5
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-07-06 21:59:08 +0300

    feat: set prettier and eslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+import { AppComponent } from './app.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
