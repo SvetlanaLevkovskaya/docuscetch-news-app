7f46b3774f9af4b32c3c6ad5ba395bc360dd0ff6
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-07-07 21:16:33 +0300

    feat: add css styles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: 'https://social-network.samuraijs.com/api/1.1',
   apiKey: '020eede9-536a-43e1-ba0a-ab142c34e29e',
 }
