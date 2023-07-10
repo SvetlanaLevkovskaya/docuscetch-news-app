1e9d476ff843d30f748e05117f5f4bf5537714ec
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-07-10 18:23:57 +0300

    feat: add styles for create-news component

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -9,7 +9,6 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
   async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
     const isAuth = this.authService.isAuth
-    debugger
     if (!isAuth) {
       await this.router.navigate(['/login'])
     }
