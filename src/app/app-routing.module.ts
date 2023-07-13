4d1402de6b0a42d471ac36d8966bdaef0fd52581
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-07-13 10:36:19 +0300

    fix: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,19 @@
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
 import { NgModule } from '@angular/core'
-import { authGuard } from './auth/guards/auth.guard'
-import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component'
 
 const routes: Routes = [
   {
     path: 'news',
     loadChildren: () => import('./news/news.module').then(el => el.NewsModule),
-    canActivate: [authGuard],
   },
   {
     path: 'login',
     loadChildren: () => import('./auth/auth.module').then(el => el.AuthModule),
   },
-  {
-    path: '**',
-    component: PageNotFoundComponent,
-  },
+  /*  {
+   path: '**',
+   component: PageNotFoundComponent,
+   },*/
   /*{ path: '', pathMatch: 'full', redirectTo: 'login' },*/
 ]
 
