5adde2a3f8f9604218aabd264b62fea2c014ad7c
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-07-28 12:47:14 +0300

    feat: add delay for notifications

--- src/app/shared/services/notification.service.ts
+++ src/app/shared/services/notification.service.ts
@@ -9,11 +9,25 @@ export class NotificationService {
   handleError(message: string) {
     console.log('Error notification:', message)
     this.notify$.next({ severity: 'error', message })
+
+    setTimeout(() => {
+      const currentNotification = this.notify$.getValue()
+      if (currentNotification?.message === message) {
+        this.clear()
+      }
+    }, 3000)
   }
 
   handleSuccess(message: string) {
     console.log('Success notification:', message)
     this.notify$.next({ severity: 'success', message })
+
+    setTimeout(() => {
+      const currentNotification = this.notify$.getValue()
+      if (currentNotification?.message === message) {
+        this.clear()
+      }
+    }, 3000)
   }
 
   clear() {
