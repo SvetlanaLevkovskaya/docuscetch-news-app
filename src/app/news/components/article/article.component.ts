f486de3cf6f1b850a101b38e2e58b6f6eb963b2d
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-08-01 23:35:05 +0300

    feat: add takeUntilDestroyed operator to automatically complete an observable

--- src/app/news/components/article/article.component.ts
+++ src/app/news/components/article/article.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnDestroy } from '@angular/core'
+import { Component } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { News } from '../../interfaces/news.interfaces'
 import { NgxSpinnerService } from 'ngx-spinner'
-import { Subscription } from 'rxjs'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-article',
@@ -10,19 +10,12 @@ import { Subscription } from 'rxjs'
   styleUrls: ['./article.component.css'],
   providers: [NgxSpinnerService],
 })
-export class ArticleComponent implements OnDestroy {
+export class ArticleComponent {
   article: News | undefined
-  //создается отдельный экземпляр объекта Subscription для каждой подписки на запрос.
-  //Это может быть полезно, если в компоненте есть несколько запросов с разными условиями подписки.
-  private articleSubscription: Subscription
 
   constructor(private route: ActivatedRoute) {
-    this.articleSubscription = this.route.data.subscribe(({ article }) => {
+    this.route.data.pipe(takeUntilDestroyed()).subscribe(({ article }) => {
       this.article = article
     })
-  }
-
-  ngOnDestroy(): void {
-    this.articleSubscription.unsubscribe()
   }
 }
